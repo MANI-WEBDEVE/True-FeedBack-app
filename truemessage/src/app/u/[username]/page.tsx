1a0b5c6a41a15447f60a6d4aa0627ad3db4176bb
Muhammad Inam <nudmaufon@gmail.com>
2024-09-22 14:37:47 +0500

    AI-integrate-for-app

--- truemessage/src/app/u/[username]/page.tsx
+++ truemessage/src/app/u/[username]/page.tsx
@@ -10,16 +10,21 @@ import { ApiResponse } from "@/types/ApiResponse";
 
 const ProfilePage = () => {
   const [messageContent, setMessageContent] = useState<string>("");
-  const [userAcceptMessage, setUserAcceptMessage] = useState<boolean | undefined>();
+  const [userAcceptMessage, setUserAcceptMessage] = useState< boolean | undefined>();
+  const [suggestedMessages, setSuggestedMessages] = useState<string[]>([]);
+  const [suggestedInput, setSuggestedInput] = useState<string>("");
   const { toast } = useToast();
 
   const username = window.location.pathname.split("/")[2];
 
   const handleUserAcceptMessage = async () => {
     try {
-      const response = await axios.post<ApiResponse>("/api/user-accept-message", { username });
+      const response = await axios.post<ApiResponse>(
+        "/api/user-accept-message",
+        { username }
+      );
       setUserAcceptMessage(response.data.userAcceptMessage);
-      
+
       if (response.data.userAcceptMessage) {
         toast({ title: "User Accepted Message" });
       } else {
@@ -28,7 +33,10 @@ const ProfilePage = () => {
     } catch (error) {
       console.log(error);
       const axiosErro = error as AxiosError<ApiResponse>;
-      toast({ title: "Error fetching user acceptance", description: axiosErro.response?.data.message });
+      toast({
+        title: "Error fetching user acceptance",
+        description: axiosErro.response?.data.message,
+      });
     }
   };
 
@@ -41,14 +49,37 @@ const ProfilePage = () => {
       toast({ title: "Message cannot be empty" });
       return;
     }
+  
+      try {
+        const response = await axios.post("/api/send-messages", {
+          username,
+          content: messageContent,
+        });
+        toast({ title: "Message sent successfully!" });
+      } catch (error: any) {
+        toast({ title: "Error sending message", description: error.message });
+      }
+  };
+
+  const fetchAIMessages = async () => {
     try {
-      const response = await axios.post("/api/send-messages", { username, content: messageContent });
-      console.log(response.data);
-      toast({ title: "Message sent successfully!" });
-    } catch (error: any) {
-      console.log(error.message);
-      toast({ title: "Error sending message", description: error.message });
+      const response = await axios.post("/api/suggeste-messages");
+      const result = response.data;
+      console.log(result);
+
+      const resultArray = result.result.split("||");
+      setSuggestedMessages(resultArray);
+    } catch (error) {
+      const axiosErro = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error fetching messages",
+        description: axiosErro.response?.data.message,
+      });
     }
+  };
+
+  const handleInputSugguested = (index: number) => {
+    setSuggestedInput(suggestedMessages[index]);
   };
 
   return (
@@ -72,17 +103,50 @@ const ProfilePage = () => {
         <div className="w-1/2 mx-auto mt-2">
           <Input
             placeholder="Enter Your Message"
-            value={messageContent}
+            value={messageContent ? messageContent : suggestedInput}
             onChange={(e) => setMessageContent(e.target.value)}
           />
         </div>
-        <div className="w-1/2 mx-auto mt-2">
+        <div className="w-1/2 mx-auto mt-6">
+          {userAcceptMessage ? (
+            <Button
+              onClick={sendMessageApi}
+              className="bg-purple-600 font-bold text-black px-4 py-2 hover:bg-purple-500 transition-all hover:scale-110 duration-500 uppercase"
+            >
+              Send It
+            </Button>
+          ) : (
+            <Button
+              onClick={handleUserAcceptMessage}
+              className="bg-purple-600 font-bold text-black px-4 py-2 hover:bg-purple-500 transition-all hover:scale-110 duration-500 uppercase"
+            >
+              Message Not Send
+            </Button>
+          )}
+        </div>
+        <div className="w-1/2 mx-auto mt-9">
+          <h1 className="text-4xl text-center tracking-tighter font-semibold uppercase">
+            AI Suggested Messages
+          </h1>
+        </div>
+        <div className="flex items-center justify-center mt-6 ">
           <Button
-            onClick={sendMessageApi}
-            className="bg-purple-600 font-bold text-black px-4 py-2 hover:bg-purple-500 transition-all hover:scale-110 duration-500 uppercase"
+            onClick={fetchAIMessages}
+            className="bg-purple-600 font-bold text-black px-4 py-2 hover:bg-purple-500 transition-all hover:scale-105 duration-500 uppercase"
           >
-            Send It
+            Suggested Messages
           </Button>
+        </div>
+        <div className=" h-72 w-1/2 mx-auto mt-2 bg-gray-100 flex  justify-center flex-col gap-2 ">
+          {suggestedMessages.map((message, index) => (
+            <p
+              key={index}
+              className="text-center text-lg font-medium px-3 py-2 border-[1px] border-black rounded-lg hover:scale-105 duration-300 transition-all hover:bg-purple-300 cursor-pointer "
+              onClick={() => handleInputSugguested(index)}
+            >
+              {message}
+            </p>
+          ))}
         </div>
       </main>
     </>
